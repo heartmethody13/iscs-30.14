ed257292aad8cd78bdcade8ac1df0587faaee535
heartmethody13 <157770882+heartmethody13@users.noreply.github.com>
2026-01-31 22:48:46 +0800

    latest changes

--- TileMapGame/player.gd
+++ TileMapGame/player.gd
@@ -6,16 +6,21 @@ const SPEED = 300.0
 
 func _physics_process(delta: float) -> void:
 	var horizontal_direction := Input.get_axis("ui_left", "ui_right")
+	var vertical_direction := Input.get_axis("ui_up", "ui_down")
+	
 	if horizontal_direction:
 		velocity.x = horizontal_direction * SPEED
+		velocity.y = 0
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
-	var vertical_direction := Input.get_axis("ui_up", "ui_down")
+	
 	if vertical_direction:
 		velocity.y = vertical_direction * SPEED
+		velocity.x = 0
 	else:
 		velocity.y = move_toward(velocity.y, 0, SPEED)
+		
 
 	move_and_slide()
 	update_animations(horizontal_direction, vertical_direction)
@@ -23,6 +28,7 @@ func _physics_process(delta: float) -> void:
 
 func update_animations(h_dir, v_dir):
 	if h_dir != 0:
+		
 		sprite.play("walk_side")
 		sprite.flip_h = (h_dir < 0) # Flips the sprite when moving left
 	elif v_dir > 0:
