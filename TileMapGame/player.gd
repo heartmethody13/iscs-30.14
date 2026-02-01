87c973b5436d740cf9a5f2ba1f95b80fdfb92e6c
ShaiDayag <shaan.dayag@student.ateneo.edu>
2026-02-01 16:08:34 +0800

    feat: add movement and animation transitions following specs

--- TileMapGame/player.gd
+++ TileMapGame/player.gd
@@ -1,41 +1,75 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
 @onready var sprite = $AnimatedSprite2D
+@onready var ray = $RayCast2D
 
-func _physics_process(delta: float) -> void:
-	var horizontal_direction := Input.get_axis("ui_left", "ui_right")
-	var vertical_direction := Input.get_axis("ui_up", "ui_down")
-	
-	if horizontal_direction:
-		velocity.x = horizontal_direction * SPEED
-		velocity.y = 0
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+const SPEED = 32.0
+const TILE_SIZE = 32
+const INPUTS = {"ui_left": Vector2.LEFT, "ui_right": Vector2.RIGHT, "ui_up": Vector2.UP, "ui_down": Vector2.DOWN}
+const ANIMATION_SPEED = 4
+var moving = false
+var direction_last_facing = "down"
 
+#func _physics_process(delta: float) -> void:
+	##velocity = Vector2(10,10)
+	#velocity = velocity/2
+	#move_and_slide()
+func _ready():
+	position = position.snapped(Vector2.ONE * TILE_SIZE)
+	position += Vector2.ONE * TILE_SIZE/2
 	
-	if vertical_direction:
-		velocity.y = vertical_direction * SPEED
-		velocity.x = 0
-	else:
-		velocity.y = move_toward(velocity.y, 0, SPEED)
+func _input(event) -> void:
+	if moving:
+		return
+
+	for direction in INPUTS.keys():
+		var action = INPUTS[direction]
+		if event.is_action(direction):
+			move(direction)
+
+func move(direction) -> void:
+	ray.target_position = INPUTS[direction] * TILE_SIZE
+	ray.force_raycast_update()
+	
+	if ray.is_colliding() == false:
+		#position += INPUTS[direction] * TILE_SIZE
+		var tween = get_tree().create_tween()
+		tween.tween_property(self, "position", position + INPUTS[direction] * TILE_SIZE, 1.0 / ANIMATION_SPEED).set_trans(Tween.TRANS_LINEAR)
+		#tween.tween_property(self, "position", position + INPUTS[direction] * TILE_SIZE, 1.0 / ANIMATION_SPEED).set_ease(Tween.EASE_IN_OUT)
 		
+		moving = true
+		$AnimationPlayer.play(direction)
+		update_animations(INPUTS[direction].x, INPUTS[direction].y)
+		
+		await tween.finished
+		moving = false
+		update_animations(0, 0)
 
-	move_and_slide()
-	update_animations(horizontal_direction, vertical_direction)
-	
+	else:
+		update_animations(INPUTS[direction].x, INPUTS[direction].y)
+		update_animations(0, 0)
+
 
 func update_animations(h_dir, v_dir):
+	var current_direction = null
 	if h_dir != 0:
+		current_direction = "side"
+		sprite.flip_h = (h_dir < 0)
+	elif v_dir > 0: current_direction = "down"
+	elif v_dir < 0: current_direction = "up"
+	
+	if current_direction != null:
+		direction_last_facing = current_direction
 		
-		sprite.play("walk_side")
-		sprite.flip_h = (h_dir < 0) # Flips the sprite when moving left
-	elif v_dir > 0:
-		sprite.play("walk_down")
-	elif v_dir < 0:
-		sprite.play("walk_up")
+	if h_dir == 0 and v_dir == 0:
+		sprite.play("walk_" + direction_last_facing)
+		sprite.stop()
 	else:
-		if sprite.animation:
-			sprite.stop()
-			sprite.frame = 0
+		sprite.play("walk_" + current_direction)
+		await sprite.animation_finished
+		#sprite.frame = 0
+	#else:
+		#if sprite.animation:
+			#sprite.stop()
+			#sprite.frame = 0
